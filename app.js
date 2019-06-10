7a60b2ee69478f4892116b54630b5954d2a0aeb2
FianuD <dovlofianu@gmail.com>
2019-06-10 01:46:48 +0000

    Feature: Hand shakes
    Add hand shaking when making a choice.

--- app.js
+++ app.js
@@ -23,6 +23,13 @@ const game = () => {
         const options = document.querySelectorAll('.options button');
         const playerHand = document.querySelector('.player-hand');
         const computerHand = document.querySelector('.computer-hand');
+        const hands = document.querySelectorAll('.hands img');
+
+        hands.forEach(hand => {
+            hand.addEventListener('animationend', function(){
+                this.style.animation = '';
+            });
+        });
         // computer's options
         const computerOptions = ['rock', 'paper', 'scissors'];
 
@@ -31,12 +38,18 @@ const game = () => {
             // generate random number from 0, 1 and 2 for computer choice
             const computerNumber = Math.floor(Math.random() * 3);
             const computerChoice = computerOptions[computerNumber];
-            // we call compare hands here
-            compareHands(this.textContent, computerChoice);
+            // set delay for image change after shake
+            setTimeout(() => {
+                // we call compare hands here
+                compareHands(this.textContent, computerChoice);
+                // update images to match pick
+                playerHand.src = `./assets/${this.textContent}.png`;
+                computerHand.src = `./assets/${computerChoice}.png`;
+            }, 2000)
 
-            // update images to match pick
-            playerHand.src = `./assets/${this.textContent}.png`;
-            computerHand.src = `./assets/${computerChoice}.png`;
+            // animate hands
+            playerHand.style.animation = "shakePlayer 2s ease";
+            computerHand.style.animation = "shakeComputer 2s ease";
             });
         });
     };
